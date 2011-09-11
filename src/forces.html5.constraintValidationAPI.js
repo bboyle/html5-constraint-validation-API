7f4a61fc61b56e548fc2e1e1475fe7304203e5dd
Ben Boyle <benjamins.boyle@gmail.com>
2011-09-11 18:23:22 +1000

    touch up submit tests
    cleanup after testing .bind() vs .live()  note that opera 11.5 on OSX is
    still triggering the submit event even when invalid fields are present.

--- src/forces.html5.constraintValidationAPI.js
+++ src/forces.html5.constraintValidationAPI.js
@@ -49,7 +49,7 @@ if ( jQuery !== "undefined" ) {
 
 
 		// check for blank required fields on submit
-		$( "form" ).live( "submit", function( event ) {
+		$( "form" ).live( "submit", function() {
 
 			var form = $( this );
 
